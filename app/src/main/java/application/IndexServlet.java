4d1ad37957f59822d3eef2d7b89a94c362cc8ed7
Iago Kater Menegon <126284906+iagokater@users.noreply.github.com>
2024-03-08 19:33:46 +0000

    feat: aula 3 javaweb

---
+++ app/src/main/java/application/IndexServlet.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "IndexServlet", urlPatterns = {"/"})
+public class IndexServlet extends HttpServlet{
+    public void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        String n =  request.getParameter("nome");
+        request.setAttribute("nome", n);
+        request.getRequestDispatcher(
+            "/resposta.jsp").forward(request, response);
+    }
+}
---
+++ app/src/main/webapp/resposta.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang "pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Resposta</title>
+</head>
+<body>
+    <h1>Resposta</h1>
+</body>
+</html>
